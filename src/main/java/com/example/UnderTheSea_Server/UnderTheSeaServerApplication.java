be14a340d56f84677eff064ceee61671f26706c5
wyoung163 <20201014@sungshin.ac.kr>
2022-11-12 01:09:10 +0900

    init

---
+++ src/main/java/com/example/UnderTheSea_Server/UnderTheSeaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.UnderTheSea_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderTheSeaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderTheSeaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UnderTheSea_Server/UnderTheSeaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UnderTheSea_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderTheSeaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
