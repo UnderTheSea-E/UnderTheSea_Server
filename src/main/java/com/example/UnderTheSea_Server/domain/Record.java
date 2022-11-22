0d28014d94dee29832d2283436ceea1a84fa7635
HONG MIN <haemin1107@gmail.com>
2022-11-22 12:29:16 +0900

    feat: add record info in  db

---
+++ src/main/java/com/example/UnderTheSea_Server/domain/Record.java
@@ -0,0 +1,42 @@
+package com.example.UnderTheSea_Server.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "Record")
+public class Record {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long record_id;
+
+    @Column(nullable = true)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("'UNSATISFIED'")
+    private RecordSatisfaction satisfaction;
+
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("'ACTIVE'")
+    private RecordStatus status;
+
+    @Column(nullable = true)
+    private String img_url;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date created_at;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date updated_at;
+}
---
+++ src/main/java/com/example/UnderTheSea_Server/domain/RecordSatisfaction.java
@@ -0,0 +1,5 @@
+package com.example.UnderTheSea_Server.domain;
+
+public enum RecordSatisfaction {
+    SATISFIED, UNSATISFIED
+}
