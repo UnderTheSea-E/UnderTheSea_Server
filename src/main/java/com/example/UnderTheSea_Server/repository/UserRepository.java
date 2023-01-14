bd93f4231037a6f96bd9cb5743a5e1b29a9140d3
wyoung163 <20201014@sungshin.ac.kr>
2023-01-14 23:52:30 +0900

    feat: add method of finding a user by an email

--- src/main/java/com/example/UnderTheSea_Server/repository/UserRepository.java
+++ src/main/java/com/example/UnderTheSea_Server/repository/UserRepository.java
@@ -3,15 +3,13 @@ package com.example.UnderTheSea_Server.repository;
 import com.example.UnderTheSea_Server.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
+public interface UserRepository extends JpaRepository<User, Long>{
     // Jpa Naming 전략
     // SELECT * FROM member WHERE id = ?
     // Insert into member ( user_id, password, user_name, createdate, email, role) values ( 'hermeswing', '$2a$10$C4BZPH4raAlKGrvy9dtyyufBp56af2W6fge0hD1wLctWvNEjrK.AG', '홍길동', now(), 'hermeswing@test.com', 'ROLE_ADMIN')
     // id:1, pw:1234
     User findByUserId(String userId); // JPA Query Method
 
-    Optional<Object> findByUserEmail(String kakaoEmail);
+    User findByUserEmail(String kakaoEmail);
 }
+
