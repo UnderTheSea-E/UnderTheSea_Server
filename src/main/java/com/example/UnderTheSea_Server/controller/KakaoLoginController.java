91645183994c16a1a34e6ba8fa53395ddd7cf273
wyoung163 <20201014@sungshin.ac.kr>
2023-01-16 14:52:26 +0900

    fix: change type of mapping annotation and url

--- src/main/java/com/example/UnderTheSea_Server/controller/KakaoLoginController.java
+++ src/main/java/com/example/UnderTheSea_Server/controller/KakaoLoginController.java
@@ -3,21 +3,18 @@ package com.example.UnderTheSea_Server.controller;
 import com.example.UnderTheSea_Server.model.PostUserReq;
 import com.example.UnderTheSea_Server.service.KakaoUserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
-
 public class KakaoLoginController {
     private final KakaoUserService kakaoUserService;
 
-    @GetMapping("/user/kakao/callback")
-    public KakaoUserInfoDto kakaoLogin(@RequestParam PostUserReq postUserReq, HttpServletResponse response) throws JsonProcessingException {
+    @RequestMapping(value = "/login/kakao")
+    public KakaoUserInfoDto kakaoLogin(@RequestBody PostUserReq postUserReq, HttpServletResponse response) throws JsonProcessingException {
         return kakaoUserService.kakaoLogin(postUserReq, response);
     }
 
