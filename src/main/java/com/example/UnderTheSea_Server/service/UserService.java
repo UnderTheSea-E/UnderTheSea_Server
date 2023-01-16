fbd6eb50b160ea885e1230d9794530733576a47a
wyoung163 <20201014@sungshin.ac.kr>
2023-01-16 14:54:28 +0900

    fix: add a correct data type

--- src/main/java/com/example/UnderTheSea_Server/service/UserService.java
+++ src/main/java/com/example/UnderTheSea_Server/service/UserService.java
@@ -20,7 +20,7 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUserId(username);
+        return (UserDetails) userRepository.findByUserId(username);
                 //.orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
     }
 }
