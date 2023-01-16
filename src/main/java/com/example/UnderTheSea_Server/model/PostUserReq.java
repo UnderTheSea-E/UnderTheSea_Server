d4348eb360d2c3e70a7fe23bf3b045aa7d3e3d06
wyoung163 <20201014@sungshin.ac.kr>
2023-01-16 14:53:52 +0900

    fix: add constuctor annotation as a default constructor

--- src/main/java/com/example/UnderTheSea_Server/model/PostUserReq.java
+++ src/main/java/com/example/UnderTheSea_Server/model/PostUserReq.java
@@ -1,8 +1,10 @@
 package com.example.UnderTheSea_Server.model;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class PostUserReq {
     public String accessToken;
     public String accessExpiration;
