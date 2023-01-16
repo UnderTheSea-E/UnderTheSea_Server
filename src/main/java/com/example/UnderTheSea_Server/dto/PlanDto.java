fbfe1b574840af4d892a701b7f66213e9d7ff908
wyoung163 <20201014@sungshin.ac.kr>
2023-01-16 15:45:31 +0900

    docs: hide or change some unnecessary codes temporarily

--- src/main/java/com/example/UnderTheSea_Server/dto/PlanDto.java
+++ src/main/java/com/example/UnderTheSea_Server/dto/PlanDto.java
@@ -1,28 +1,28 @@
-package com.example.UnderTheSea_Server.dto;
-
-import com.example.UnderTheSea_Server.domain.Friend;
-import com.example.UnderTheSea_Server.domain.Plan;
-import com.example.UnderTheSea_Server.domain.User;
-import org.springframework.stereotype.Repository;
-
-import java.sql.Timestamp;
-import java.util.Date;
-
-@Repository
-public class PlanDto {
-    private Timestamp created_at = new Timestamp(new Date().getTime());
-    private Timestamp updated_at = new Timestamp(new Date().getTime());
-
-    public Plan insertPlan(User user, Friend freind, Long recommend_id, String content, Date date) {
-        Plan planEntity = Plan.builder()
-                .user(user)
-                .friend(freind)
-                .recommend(recommend_id)
-                .content(content)
-                .date(date)
-                .created_at(created_at)
-                .updated_at(updated_at)
-                .build();
-        return planEntity;
-    }
-}
+//package com.example.UnderTheSea_Server.dto;
+//
+//import com.example.UnderTheSea_Server.domain.Friend;
+//import com.example.UnderTheSea_Server.domain.Plan;
+//import com.example.UnderTheSea_Server.domain.User;
+//import org.springframework.stereotype.Repository;
+//
+//import java.sql.Timestamp;
+//import java.util.Date;
+//
+//@Repository
+//public class PlanDto {
+//    private Timestamp created_at = new Timestamp(new Date().getTime());
+//    private Timestamp updated_at = new Timestamp(new Date().getTime());
+//
+//    public Plan insertPlan(User user, Friend freind, Long recommend_id, String content, Date date) {
+//        Plan planEntity = Plan.builder()
+//                .user(user)
+//                .friend(freind)
+//                .recommend(recommend_id)
+//                .content(content)
+//                .date(date)
+//                .created_at(created_at)
+//                .updated_at(updated_at)
+//                .build();
+//        return planEntity;
+//    }
+//}
