f99c75d214411585f3ad760eb0628264244bb14e
wyoung163 <20201014@sungshin.ac.kr>
2023-01-22 21:51:10 +0900

    fix: solve merge conflicts

--- src/main/java/com/example/UnderTheSea_Server/repository/UserRepository.java
+++ src/main/java/com/example/UnderTheSea_Server/repository/UserRepository.java
@@ -8,7 +8,7 @@ public interface UserRepository extends JpaRepository<User, Long>{
     // SELECT * FROM member WHERE id = ?
     // Insert into member ( user_id, password, user_name, createdate, email, role) values ( 'hermeswing', '$2a$10$C4BZPH4raAlKGrvy9dtyyufBp56af2W6fge0hD1wLctWvNEjrK.AG', '홍길동', now(), 'hermeswing@test.com', 'ROLE_ADMIN')
     // id:1, pw:1234
-    User findByUserId(String userId); // JPA Query Method
+    User findByUserId(Long userId); // JPA Query Method
 
     User findByEmail(String kakaoEmail);
 }
