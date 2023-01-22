9b4d232b828257fd4e0294cd1d9658dafa3a3bd4
wyoung163 <20201014@sungshin.ac.kr>
2023-01-22 19:04:43 +0900

    chore: remark of checking token information

--- src/main/java/com/example/UnderTheSea_Server/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/UnderTheSea_Server/jwt/JwtAuthenticationFilter.java
@@ -21,6 +21,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         //헤더에서 jwt 받아오기
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        //System.out.println("token: " + token);
         //유효한 토큰인지 확인
         if(token != null && jwtTokenProvider.validateToken(token)){
             //토큰 유효하면 토큰으로부터 유저 정보 받아오기
