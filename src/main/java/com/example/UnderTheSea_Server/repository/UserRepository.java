31d82f042484d57ee7d5cc43f8ed163abe142932
wyoung163 <20201014@sungshin.ac.kr>
2023-01-28 15:20:41 +0900

    feat: add service and repository of inserting character with update query

--- src/main/java/com/example/UnderTheSea_Server/repository/UserRepository.java
+++ src/main/java/com/example/UnderTheSea_Server/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.example.UnderTheSea_Server.repository;
 
 import com.example.UnderTheSea_Server.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long>{
     // Jpa Naming 전략
@@ -11,5 +15,13 @@ public interface UserRepository extends JpaRepository<User, Long>{
     User findByUserId(Long userId); // JPA Query Method
 
     User findByEmail(String kakaoEmail);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u set u.characterId = :character_id, characterName = :character_name where u.userId = :user_id")
+    void updateCharacterId(@Param("character_id") Long character_id,
+                                @Param("character_name") String character_name,
+                                @Param("user_id") Long user_id
+    );
 }
 
