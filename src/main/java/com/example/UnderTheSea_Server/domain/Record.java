f715ac6d97329a4cc2d8badccad69267e023939b
wyoung163 <choiwy0906@gmail.com>
2023-03-29 02:07:24 +0900

    docs: add columns and builder

--- src/main/java/com/example/UnderTheSea_Server/domain/Record.java
+++ src/main/java/com/example/UnderTheSea_Server/domain/Record.java
@@ -1,5 +1,6 @@
 package com.example.UnderTheSea_Server.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,12 @@ import org.hibernate.annotations.ColumnDefault;
 
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
 @Getter
+@Builder
 @Setter
 @NoArgsConstructor
 @Table(name = "Record")
@@ -19,6 +22,11 @@ public class Record {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long record_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
 
     @Column(nullable = true)
     private String content;
@@ -33,6 +41,9 @@ public class Record {
     @Column(nullable = true)
     private String img_url;
 
+    @Column(nullable = false)
+    private LocalDate date;
+
     @Temporal(value = TemporalType.TIMESTAMP)
     @Column(nullable = false)
     private Date created_at;
@@ -41,12 +52,14 @@ public class Record {
     @Column(nullable = false)
     private Date updated_at;
 
-    public Record(Long record_id, String content, int satisfaction, RecordStatus status, String img_url, Date created_at, Date updated_at) {
+    public Record(Long record_id, User user, String content, int satisfaction, RecordStatus status, String img_url, LocalDate date, Date created_at, Date updated_at) {
         this.record_id = record_id;
+        this.user = user;
         this.content = content;
         this.satisfaction = satisfaction;
         this.status = status;
         this.img_url = img_url;
+        this.date = date;
         this.created_at = created_at;
         this.updated_at = updated_at;
     }
