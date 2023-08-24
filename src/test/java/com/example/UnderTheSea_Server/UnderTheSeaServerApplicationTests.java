f346f8a678ec364c2c4d1b6a1161c4f13e7c11b3
wyoung163 <20201014@sungshin.ac.kr>
2023-08-24 19:56:52 +0900

    test: add unit test of saving user using springboottest+(real)db

--- src/test/java/com/example/UnderTheSea_Server/UnderTheSeaServerApplicationTests.java
+++ src/test/java/com/example/UnderTheSea_Server/UnderTheSeaServerApplicationTests.java
@@ -7,12 +7,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+// 실제 application.yaml 내용 기반으로 db 연결 및 활용
+@SpringBootTest
+// 실제 db 없이 인 메모리 db 활용
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UnderTheSeaServerApplicationTests {
 	@Autowired
 	private UserRepository userRepository;
@@ -22,7 +27,7 @@ class UnderTheSeaServerApplicationTests {
 		//given
 		String email = "wyoung@gmail.com";
 		User user = User.builder()
-				.user_id(13l)
+				//.user_id(13l)
 				.nickname("woo")
 				.email("wyoung@gmail.com")
 				.updated_at(new Date())
