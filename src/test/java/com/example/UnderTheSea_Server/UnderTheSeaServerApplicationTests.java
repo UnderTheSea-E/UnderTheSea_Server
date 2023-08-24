7af52b5aab39bfd0e2170678e1cea8f6bf61e101
wyoung163 <20201014@sungshin.ac.kr>
2023-08-24 14:52:34 +0900

    test: add unit test of saving user

--- src/test/java/com/example/UnderTheSea_Server/UnderTheSeaServerApplicationTests.java
+++ src/test/java/com/example/UnderTheSea_Server/UnderTheSeaServerApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.UnderTheSea_Server;
 
+import com.example.UnderTheSea_Server.domain.User;
+import com.example.UnderTheSea_Server.domain.UserStatus;
+import com.example.UnderTheSea_Server.repository.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.Date;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UnderTheSeaServerApplicationTests {
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
+		//given
+		String email = "wyoung@gmail.com";
+		User user = User.builder()
+				.user_id(13l)
+				.nickname("woo")
+				.email("wyoung@gmail.com")
+				.updated_at(new Date())
+				.created_at(new Date())
+				.profileImgUrl("http")
+				.status(UserStatus.ACTIVE)
+				.build();
+
+		//when
+		User saved = userRepository.save(user);
+
+		//then
+		assertThat(saved.getEmail()).isEqualTo(email);
 	}
 
 }
